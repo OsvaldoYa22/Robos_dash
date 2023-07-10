de3cb2aae838cde34bcab79343f38a00c00a9c4f
Osvaldo Yañez <osvaldoypg21@gmail.com>
2023-07-10 10:48:15 -0600

    add server

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css',
                         [dbc.themes.BOOTSTRAP]]
 # Inicializa la app Dash
 app = Dash(__name__, external_stylesheets = external_stylesheets, suppress_callback_exceptions = True)
+server = app.server
 # Diseño de la app
 app.layout = html.Div([
     html.Div([
